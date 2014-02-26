8f9ae18b157eb408aa58123e2aea94626c6f45ed
Eric Lin <ericlin@axiomzen.co>
2014-02-25 17:32:57 -0800

    Still works without

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,10 +8,6 @@ angular.module('myApp.controllers', []).
     // http://stsc3000.github.io/blog/2013/10/26/a-tale-of-frankenstein-and-binding-to-service-values-in-angular-dot-js/
     $scope.todos = Todos.collection;
 
-    $scope.$watch(function () {
-      return Todos.collection;
-    });
-
     this.complete = function (todo) {
       // Toggle 'completeness' of todo item
       todo.complete = (todo.complete === true) ? false : true;
